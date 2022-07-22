0c2413880ff91fb75b5934ae11d772c35f6b4706
JavaharlalN <grechanichenkos@yandex.ru>
2022-07-22 18:16:41 +0300

    Move shaders to the new mod

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+mod shader;
 use miniquad::*;
-
 use glam::{vec3, Mat4};
+use shader::*;
 
 struct Stage {
     display_pipeline: Pipeline,
@@ -21,45 +22,45 @@ impl Stage {
 
         #[rustfmt::skip]
         let vertices: &[f32] = &[
-            /* pos               color                   uvs */
-            -1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     0.0, 0.0,
-             1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     1.0, 0.0,
-             1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     1.0, 1.0,
-            -1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     0.0, 1.0,
+            /* pos               color            */
+            -1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,
+             1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,
+             1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,
+            -1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,
 
-            -1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     0.0, 0.0,
-             1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     1.0, 0.0,
-             1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     1.0, 1.0,
-            -1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     0.0, 1.0,
+            -1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 0.5,
+             1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 0.0,
+             1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 0.0,
+            -1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 0.0,
 
-            -1.0, -1.0, -1.0,    0.5, 0.5, 1.0, 1.0,     0.0, 0.0,
-            -1.0,  1.0, -1.0,    0.5, 0.5, 1.0, 1.0,     1.0, 0.0,
-            -1.0,  1.0,  1.0,    0.5, 0.5, 1.0, 1.0,     1.0, 1.0,
-            -1.0, -1.0,  1.0,    0.5, 0.5, 1.0, 1.0,     0.0, 1.0,
+            -1.0, -1.0, -1.0,    0.5, 0.5, 1.0, 1.0,
+            -1.0,  1.0, -1.0,    0.5, 0.5, 1.0, 1.0,
+            -1.0,  1.0,  1.0,    0.5, 0.5, 1.0, 1.0,
+            -1.0, -1.0,  1.0,    0.5, 0.5, 1.0, 1.0,
 
-             1.0, -1.0, -1.0,    1.0, 0.5, 0.0, 1.0,     0.0, 0.0,
-             1.0,  1.0, -1.0,    1.0, 0.5, 0.0, 1.0,     1.0, 0.0,
-             1.0,  1.0,  1.0,    1.0, 0.5, 0.0, 1.0,     1.0, 1.0,
-             1.0, -1.0,  1.0,    1.0, 0.5, 0.0, 1.0,     0.0, 1.0,
+             1.0, -1.0, -1.0,    1.0, 0.5, 0.0, 1.0,
+             1.0,  1.0, -1.0,    1.0, 0.5, 0.0, 1.0,
+             1.0,  1.0,  1.0,    1.0, 0.5, 0.0, 1.0,
+             1.0, -1.0,  1.0,    1.0, 0.5, 0.0, 1.0,
 
-            -1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,     0.0, 0.0,
-            -1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,     1.0, 0.0,
-             1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,     1.0, 1.0,
-             1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,     0.0, 1.0,
+            -1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,
+            -1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,
+             1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,
+             1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,
 
-            -1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,     0.0, 0.0,
-            -1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,     1.0, 0.0,
-             1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,     1.0, 1.0,
-             1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,     0.0, 1.0
+            -1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,
+            -1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,
+             1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,
+             1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,
         ];
 
         let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
 
         #[rustfmt::skip]
         let indices: &[u16] = &[
-            0, 1, 2,  0, 2, 3,
-            6, 5, 4,  7, 6, 4,
-            8, 9, 10,  8, 10, 11,
+             0,  1,  2,   0,  2,  3,
+             6,  5,  4,   7,  6,  4,
+             8,  9, 10,   8, 10, 11,
             14, 13, 12,  15, 14, 12,
             16, 17, 18,  16, 18, 19,
             22, 21, 20,  23, 22, 20
@@ -87,23 +88,12 @@ impl Stage {
             &[
                 VertexAttribute::new("pos", VertexFormat::Float3),
                 VertexAttribute::new("color0", VertexFormat::Float4),
-                VertexAttribute::new("uv0", VertexFormat::Float2),
             ],
             default_shader,
 			PipelineParams {
 				cull_face: CullFace::Nothing,
 				depth_test: Comparison::LessOrEqual,
 				depth_write: true,
-				color_blend: Some(BlendState::new(
-					Equation::Add,
-					BlendFactor::Value(BlendValue::SourceAlpha),
-					BlendFactor::OneMinusValue(BlendValue::SourceAlpha)
-				)),
-				alpha_blend: Some(BlendState::new(
-					Equation::Add,
-					BlendFactor::Zero,
-					BlendFactor::One)
-				),
 				primitive_type: PrimitiveType::Triangles,
 				..Default::default()
 			}
@@ -153,46 +143,4 @@ fn main() {
         },
         |mut ctx| Box::new(Stage::new(&mut ctx)),
     );
-}
-
-mod display_shader {
-    use miniquad::*;
-
-    pub const VERTEX: &str = r#"#version 100
-    attribute vec4 pos;
-    attribute vec4 color0;
-    attribute vec2 uv0;
-    varying lowp vec4 color;
-    varying lowp vec2 uv;
-    uniform mat4 mvp;
-    void main() {
-        gl_Position = mvp * pos;
-        color = color0;
-        uv = uv0;
-    }
-    "#;
-
-    pub const FRAGMENT: &str = r#"#version 100
-    varying lowp vec4 color;
-    varying lowp vec2 uv;
-    // uniform sampler2D tex;
-    void main() {
-        gl_FragColor = color;
-        // gl_FragColor = color * texture2D(tex, uv);
-    }
-    "#;
-
-    pub fn meta() -> ShaderMeta {
-        ShaderMeta {
-            images: vec!["tex".to_string()],
-            uniforms: UniformBlockLayout {
-                uniforms: vec![UniformDesc::new("mvp", UniformType::Mat4)],
-            },
-        }
-    }
-
-    #[repr(C)]
-    pub struct Uniforms {
-        pub mvp: glam::Mat4,
-    }
 }
